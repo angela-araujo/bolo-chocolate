9accf60cd8fa83acd4d8861a376ec34b792af77f
Angela de Araujo <angela.araujo.rj@gmail.com>
2023-09-06 16:06:38 -0300

    primeiro commit

---
+++ src/test/java/acc/br/BoloIT.java
@@ -0,0 +1,8 @@
+package acc.br;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class BoloIT extends BoloTest {
+    // Execute the same tests but in packaged mode.
+}
